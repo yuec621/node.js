c4a2f9724f7ab8a0c989e326eda4722a33259f80
yuec621 <yuec621@outlook.com>
2019-03-15 11:24:14 +0800

    命令行参数

--- cmd.js
+++ cmd.js
@@ -1,2 +1,21 @@
-const arg=process.argv[2];
-console.log(arg);
+let i=0
+function a(){
+    while(i<10){
+        i+=1;
+
+    }
+}
+function b(){
+    a();
+
+}
+b();
+console.log(i);
+
+/**
+ * 客户端附加上才运行
+ * node --inspect-brk cmd.js
+ * 
+ * 
+ *help
+ */
